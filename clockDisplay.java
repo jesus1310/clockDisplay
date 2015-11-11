0e3ba54c2942de8695f0c8161a82eca8006fc81a
Jesus <jfr1013@gmail.com>
2015-11-11 12:23:27 +0100

    Añadido método para fijar la hora

--- clockDisplay.java
+++ clockDisplay.java
@@ -1,33 +1,36 @@
-
 /**
- * Write a description of class clockDisplay here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Imaginemos que tenemos un amigo programador en Java como nosotros 
+ * que quiere hacer una aplicación para teléfonos Android 
+ * consistente en un reloj en formato 24 horas, 
+ * que muestre las horas y minutos desde las 00:00 hasta las 23:59.
  */
 public class clockDisplay
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    // Atributo para controlar las horas
+    private int horas;
+    // Atributo para controlar los minutos
+    private int minutos;
+ 
     /**
      * Constructor for objects of class clockDisplay
      */
     public clockDisplay()
     {
-        // initialise instance variables
-        x = 0;
+        horas = 0;
+        minutos = 0;
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Metodo que permite fijar la hora y los minutos.
      */
-    public int sampleMethod(int y)
+    public void setTime(int hora,int min)
     {
-        // put your code here
-        return x + y;
+        if (hora <= 23 & hora >= 0 & min >= 0 & min <= 59){
+            horas = hora;
+            minutos = min;
+        }
+        else{
+            System.out.println("La hora introducida no es válida");
+        }
     }
 }
